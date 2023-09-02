3e2981566798663e49876747f06e4dff2aa09598
sebas arriola <sebdeveloper6952@gmail.com>
2023-09-01 19:39:52 -0600

    some otel shit in the env file

--- conxole/otel/uptrace.go
+++ conxole/otel/uptrace.go
@@ -8,11 +8,16 @@ import (
 )
 
 func InitUptrace() {
+	environment := "prod"
+	if os.Getenv("PROD") == "false" {
+		environment = "local"
+	}
+
 	uptrace.ConfigureOpentelemetry(
 		uptrace.WithDSN(os.Getenv("UPTRACE_DSN")),
 		uptrace.WithServiceName("conxole-relay"),
 		uptrace.WithServiceVersion("v1.0.0"),
-		uptrace.WithDeploymentEnvironment("local"),
+		uptrace.WithDeploymentEnvironment(environment),
 	)
 }
 
