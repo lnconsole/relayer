57be4c1bfeae0a9c99b2014af66dd999096b0d5e
cj-ibex <chongjin@ibexmercado.com>
2023-09-20 22:34:30 -0600

    remove background kind

--- conxole/main.go
+++ conxole/main.go
@@ -228,7 +228,7 @@ func main() {
 				nostr.KindChannelMessage, // 42
 				nostr.KindZap,            // 9735
 				65000, 65001,
-				65005, 65010, 65011, 65012,
+				65005, 65007, 65011, 65012,
 				31990,
 			},
 			Since: &now,
